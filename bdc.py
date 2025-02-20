f552176819cde13937de241f46a6e42104b89055
W. Nick Pappin <nick.pappin@wsu.edu>
2025-02-20 09:46:09 -0800

    Finished availabilty, and added caching to function.

--- bdc.py
+++ bdc.py
@@ -9,9 +9,10 @@ Created on Feb 10 2025.
 Updated on Feb 11 2025.
 """
 
-from . import logger, session, metadata
+from . import logger, session, metadata, bdcCache
 import pandas as pd
-import json
+import json, zipfile, io
+from pprint import pprint
 
 # from . import config
 
@@ -19,44 +20,72 @@ import json
 class availability:
 
     def state(
-        states: int | str | list = 53,
-        technology: int | str | list = 50,
+        states: int | str | list = "53",
+        technology: int | str | list = "50",
         release: str | list = "2024-06-30",
+        cache=False,
     ) -> pd.DataFrame:
         logger.info("Collecting availability...")
         logger.debug(f"State: {states}")
         logger.debug(f"Technology: {technology}")
         logger.debug(f"Release: {release}")
         if type(states) is not list:
-            state = [states]
+            states = [states]
         if type(technology) is not list:
             technology = [technology]
         if type(release) is not list:
             release = [release]
-        # Add normalization code here
+        # TODO: Add normalization code here
 
         # Retrieve availability data
         availability = dict()
         for r in release:
             response = session.get(
-                f"https://broadbandmap.fcc.gov/api/publvic/map/downloads/listAvailabilityData/{r}"
+                f"https://broadbandmap.fcc.gov/api/public/map/downloads/listAvailabilityData/{r}"
             )
             if response.status_code != 200:
                 logger.error(f"Failed to retrieve availability data for {r}.")
-                return response
                 raise Exception(f"Failed to retrieve availability data for {r}.")
-            availability[r] = pd.DataFrame.from_dict(response.json())
-        return availability
+            # TODO: adding dtype hints here I think would be helpful.
+            availability[r] = pd.DataFrame.from_dict(response.json()["data"])
+        df = pd.DataFrame()
         for r in release:
             rlocal = availability[r]
-            for s in states:
-                for t in technology:
-                    logger.debug(f"State: {s}, Technology: {t}, Release: {r}")
-
-                    # session.get("https://api.bdc.com/availability")
-        logger.debug(f"State: {state}, Technology: {technology}, Release: {release}")
-        df = pd.DataFrame()
-        # Your code here
+            items = rlocal[
+                (rlocal.category == "State")
+                & (rlocal.state_fips.isin(states))
+                & (rlocal.technology_code.isin(technology))
+            ].to_dict("records")
+            for item in items:
+                logger.debug(
+                    f"State: {item['state_name']}, Technology: {item['technology_code']}, Release: {r}"
+                )
+                # FUCK I DONT LIKE THIS
+                if cache and bdcCache.check(item["file_name"]):
+                    data = bdcCache.get(item["file_name"])
+                    logger.debug("Cache hit!")
+                    pass
+                else:
+                    response = session.get(
+                        f"https://broadbandmap.fcc.gov/api/public/map/downloads/downloadFile/availability/{item['file_id']}"
+                    )
+                    if response.status_code == 200 and cache:
+                        bdcCache.save(item["file_name"], response.content)
+                    data = response.content
+                if response.status_code != 200:
+                    logger.error(
+                        f"Failed to retrieve availability data for {item['state_name']} and {item['technology_code']} in {r}."
+                    )
+                    raise Exception(
+                        f"Failed to retrieve availability data for {item['state_name']} and {item['technology_code']} in {r}."
+                    )
+                else:
+                    zip = zipfile.ZipFile(io.BytesIO(data))
+                    localdf = pd.read_csv(
+                        zip.open(zip.filelist[0].filename)
+                    )  # , dtype=columnHints)
+                    df = pd.concat([df, localdf], ignore_index=True)
+        logger.debug(f"State: {states}, Technology: {technology}, Release: {release}")
         return df
 
 
