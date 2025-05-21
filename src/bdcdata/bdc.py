be697c4d39013e9c1fa0cacdb6e931c452b25d94
W. Nick Pappin <nick.pappin@wsu.edu>
2025-05-21 09:11:11 -0700

    Ran Black.

--- src/bdcdata/bdc.py
+++ src/bdcdata/bdc.py
@@ -23,7 +23,7 @@ class availability:
     A class to retrieve broadband availability data for specified states, technologies, and release dates.
     """
 
-    def state(
+    def fixed(
         states: int | str | list = "53",
         technology: int | str | list = "50",
         release: str | list = "2024-06-30",
@@ -33,10 +33,10 @@ class availability:
         Retrieves broadband availability data for specified states, technologies, and release dates.
 
         Args:
-            states (int | str | list, optional): State FIPS code(s) to filter by. 
+            states (int | str | list, optional): State FIPS code(s) to filter by.
                 Can be a single value, a list of values, or "all" to include all states. Defaults to "53".
-            technology (int | str | list, optional): Technology code(s) to filter by. 
-                Can be a single value, a list of values, "all" to include all technologies, 
+            technology (int | str | list, optional): Technology code(s) to filter by.
+                Can be a single value, a list of values, "all" to include all technologies,
                 "fixed" for fixed technologies, or "mobile" for mobile technologies. Defaults to "50".
             release (str | list, optional): Release date(s) to filter by. Can be a single value or a list of values. Defaults to "2024-06-30".
             cache (bool, optional): Whether to use caching for downloaded files. Defaults to False.
@@ -53,11 +53,11 @@ class availability:
         logger.debug(f"State: {states}")
         logger.debug(f"Technology: {technology}")
         logger.debug(f"Release: {release}")
-        
+
         # TODO: Add empty detection here
         if isEmpty(states) or isEmpty(technology) or isEmpty(release):
             raise Exception("One or more parameters are empty.")
-        
+
         # Normalization code
         if type(states) is not list:
             states = [states]
@@ -82,35 +82,57 @@ class availability:
         if "all" in states:
             states = availability[r].state_fips.drop_duplicates().dropna().tolist()
         if "all" in technology:
-            technology = availability[r].technology_code.drop_duplicates().dropna().tolist()
+            technology = (
+                availability[r].technology_code.drop_duplicates().dropna().tolist()
+            )
         elif "fixed" in technology:
-            technology = availability[r][((availability[r].subcategory == "Location Coverage") & (availability[r].provider_id.isnull()))].technology_code.drop_duplicates().dropna().tolist()
+            technology = (
+                availability[r][
+                    (
+                        (availability[r].subcategory == "Location Coverage")
+                        & (availability[r].provider_id.isnull())
+                    )
+                ]
+                .technology_code.drop_duplicates()
+                .dropna()
+                .tolist()
+            )
             technology = [t for t in technology if int(t) < 100]
         elif "mobile" in technology:
-            technology = availability[r][((availability[r].subcategory == "Location Coverage") & (availability[r].provider_id.isnull()))].technology_code.drop_duplicates().dropna().tolist()
+            technology = (
+                availability[r][
+                    (
+                        (availability[r].subcategory == "Location Coverage")
+                        & (availability[r].provider_id.isnull())
+                    )
+                ]
+                .technology_code.drop_duplicates()
+                .dropna()
+                .tolist()
+            )
             technology = [t for t in technology if int(t) >= 100]
-        
+
         df = pd.DataFrame()
         columnHints = {
-            'frn': str,
-            'provider_id': 'UInt32',
-            'brand_name': str,
-            'location_id': 'UInt32',
-            'technology': 'UInt16',
-            'max_advertised_download_speed': 'UInt32',
-            'max_advertised_upload_speed': 'UInt32',
-            'low_latency': 'boolean',
-            'business_residental_code': 'category',
-            'state_usps': 'category',
-            'block_geoid': str,
-            'h3_res8_id': str,
+            "frn": str,
+            "provider_id": "UInt32",
+            "brand_name": str,
+            "location_id": "UInt32",
+            "technology": "UInt16",
+            "max_advertised_download_speed": "UInt32",
+            "max_advertised_upload_speed": "UInt32",
+            "low_latency": "boolean",
+            "business_residental_code": "category",
+            "state_usps": "category",
+            "block_geoid": str,
+            "h3_res8_id": str,
         }
-        
+
         if len(release) * len(states) * len(technology) > 100:
             logger.warning(
                 f"Retrieving {len(release) * len(states) * len(technology)} records. This may take a while... or crash."
             )
-        
+
         for r in release:
             rlocal = availability[r]
             items = rlocal[
@@ -144,12 +166,14 @@ class availability:
                 else:
                     zip = zipfile.ZipFile(io.BytesIO(data))
                     localdf = pd.read_csv(
-                        zip.open(zip.filelist[0].filename)
-                    , dtype=columnHints,
-                    dtype_backend="pyarrow"
+                        zip.open(zip.filelist[0].filename),
+                        dtype=columnHints,
+                        dtype_backend="pyarrow",
                     )
                     df = pd.concat([df, localdf], ignore_index=True)
-                logger.info(f"df memory size (hinted): {df.memory_usage(deep=True).sum()/1000000} MB")
+                logger.info(
+                    f"df memory size (hinted): {df.memory_usage(deep=True).sum()/1000000} MB"
+                )
                 logger.info(f"df shape: {df.shape}")
         logger.debug(f"State: {states}, Technology: {technology}, Release: {release}")
         return df
